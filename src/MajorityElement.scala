749272bd2ca3fb978239a33f86a0f612bca1550c
Chris Cooper <chris@quixotic-chris.com>
2015-04-06 14:43:51 -0400

    actually added majority element

---
+++ src/MajorityElement.scala
@@ -0,0 +1,35 @@
+import _root_.runtime.RosalindSolution
+
+import collection.mutable
+import math.ceil
+
+object MajorityElement extends RosalindSolution {
+
+  def problem_name: String = "maj"
+
+  def get_answer(input_lines: Iterator[String]): String = {
+
+    // Ignore metadata
+    val metadata = input_lines.next().split("\\s").map{_.toInt}
+    val array_lengths = metadata(1)
+    val threshold = ceil(array_lengths / 2.0)
+
+    val arrays = input_lines.map{_.split("\\s").map{_.toInt}}
+
+    val majority_elems = arrays.map{ arr =>
+      val counts = mutable.Map[Int, Int]().withDefaultValue(0)
+
+      var maj = -1
+      for (i <- arr) {
+        val new_count = counts(i) + 1
+        counts.update(i, new_count)
+        if (new_count > threshold) {
+          maj = i
+        }
+      }
+      maj
+    }
+
+    majority_elems.mkString(" ")
+  }
+}
